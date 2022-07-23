b4985167724a53ba627f79cf6c75c94f5b76350a
EC2 Default User <ec2-user@ip-172-31-38-126.ap-northeast-1.compute.internal>
2022-07-23 12:02:30 +0000

    [Finish 3-7]

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -27,10 +27,16 @@ class ListsController < ApplicationController
     list.update(list_params)
     redirect_to list_path(list.id)
   end
+  
+  def destroy
+    list=List.find(params[:id])
+    list.destroy
+    redirect_to '/lists'
+  end
 
   private
   
   def list_params
-    params.require(:list).permit(:title,:body)
+    params.require(:list).permit(:title,:body,:image)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  get 'lists/new'
+
   get '/top'=>'homes#top'
-  post 'lists'=>'lists#create'
-  get 'lists'=>'lists#index'
-  get 'lists/:id'=>'lists#show',as:'list'
-  get 'lists/:id/edit'=>'lists#edit',as:'edit_list'
-  patch 'lists/:id'=>'lists#update',as:'update_list'
+  resources :lists
+
 end
